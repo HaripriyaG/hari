b0427168509bbd5d1297554b4b580d75b1700a94
HaripriyaG <haripriyalavendar@gmail.com>
2016-09-01 17:38:10 +0530

    Add files via upload

---
+++ days.java
@@ -0,0 +1,19 @@
+package string;
+import java.util.*;
+public class days {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner s=new Scanner(System.in);
+String a=s.next();
+if(a.equals("sunday"))
+{
+	System.out.println("false");
+}
+else
+{
+	System.out.println("true");
+}
+	}
+
+}
